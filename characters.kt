292c3f1a2931020dd81bb878a2aca4535cdc52fb
Beregszászi István <beistvan@gmail.com>
2023-08-06 12:06:32 +0200

    Characters

---
+++ characters.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Characters:")
+    
+    val aChar: Char = 'a'
+    
+    println(aChar)
+    println("Tabulator character \\t:")
+    print('\t')
+    println("Some test after tab.")
+    println("New line (LF) character \\n:")
+    println('\n')
+    println("Carriage return (CR) character \\r:")
+    println('\r')
+    println("Backspace character \\b:")
+    println('\b')
+    println('\'')
+    println('\"')
+    println('\\')
+    println('\$')
+    println('\uFF00') // UTF char
+}
