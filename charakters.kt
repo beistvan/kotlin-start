5424eef1952e5afdc60c42753e1066507d94f3c8
Beregszászi István <beistvan@gmail.com>
2023-08-06 11:53:12 +0200

    Characters

---
+++ charakters.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Characters:")
+    
+    val aChar: Char = 'a'
+    
+    println(aChar)
+    println("New line character \\n:")
+    println('\n')
+    println('\uFF00')    
+}
