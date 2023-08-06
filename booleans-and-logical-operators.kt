803649b54fc4f1c2985d7ae941ba3bd511b5fb2d
Beregszászi István <beistvan@gmail.com>
2023-08-06 11:49:34 +0200

    Booleans And Logical Operators

---
+++ booleans-and-logical-operators.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Booleans And Logical Operators:")
+    
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+    val boolNull: Boolean? = null
+    
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+    println(!myTrue)
+    println(boolNull)    
+}
