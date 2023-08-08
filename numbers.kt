09889cb1bf7170f92675b45b7426f283121c3a21
Beregszászi István <beistvan@gmail.com>
2023-08-08 10:34:21 +0200

    Numbers

---
+++ numbers.kt
@@ -0,0 +1,40 @@
+fun main() {
+    println("Numbers:")
+    
+    val one = 1
+    val threeBillion = 3000000000
+    val oneLong = 1L
+    val oneByte: Byte = 1
+    val aShort: Short = 2
+    val aChar: Char = 'a'
+    
+    println("Int:")
+    println(one)
+    println("Long:")
+    println(threeBillion)
+    println("Long (one):")
+    println(oneLong)
+    println("Byte:")
+    println(oneByte)
+    println("Short:")
+    println(aShort)
+    println("Char:")
+    println(aChar)
+    
+    val pi = 3.14
+    val two: Double = 2.0 
+    val oneDouble = 1.0
+    val e = 2.7182818284
+    val eFloat = 2.7182818284f // 2.7182817
+    
+    println("Double:")
+    println(pi)
+    println("Double:")
+    println(two)
+    println("Double (one):")
+    println(oneDouble)
+    println("Double:")
+    println(e)
+    println("Float:")
+    println(eFloat)
+}
