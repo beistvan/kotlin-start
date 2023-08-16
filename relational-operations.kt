e1a554d57f22e082ba3418e62ee9999f9fbd25fd
Beregszászi István <beistvan@gmail.com>
2023-08-16 09:00:27 +0200

    Relational Operations

---
+++ relational-operations.kt
@@ -0,0 +1,33 @@
+fun main() {
+    println("Relational Operations:")
+    
+    val a = 10
+    val b = 12
+    
+    println(a)
+    println(b)
+    
+    if (a < b) {
+        println("a less, than b")
+    }
+    
+    if (a > b)  {
+        println("a greather, than b")
+    }
+    
+    if (a == b) {
+        println("a equals b")
+    }
+    
+    if (a != b)  {
+        println("a not equals b")
+    }
+    
+    if (a <= b) {
+        println("a less and equals, than b")
+    }
+    
+    if (a >= b)  {
+        println("a greather and equal, than b")
+    }
+}
