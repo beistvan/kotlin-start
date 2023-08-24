add1ebb93f1cc0568aceb644651137a2399aafc4
Beregszászi István <beistvan@gmail.com>
2023-08-24 17:50:53 +0200

    ForEach Loop

---
+++ foreach-loop.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("ForEach Loop:")
+    
+    val allStudents = listOf("Steven", "John", "Paul", "Peter", "Samantha")
+    
+    allStudents.forEach{
+        println(it)
+    }    
+}
