23dce1c318531b851ab792f3dc4396a7caa12167
beistvan <beistvan@gmail.com>
2023-08-25 20:35:22 -0300

    Name Arguments

---
+++ named-arguments.kt
@@ -0,0 +1,7 @@
+fun displayUser(name: String, surname: String, middleName: String, sex: String, nationality: String) {
+    println("Name: $name, Surname: $surname, Middle Name: $middleName, Sex: $sex, Nationality: $nationality")
+}
+
+fun main() {
+    displayUser(name = "John", surname = "Smith", middleName = "Paul", sex = "Male", nationality = "English")
+} 
