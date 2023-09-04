3eb19edb01d2992042b9c57f70b320c83a30c3da
beistvan <beistvan@gmail.com>
2023-09-04 02:40:05 +0200

    Class Mentor

---
+++ class-mentor.kt
@@ -0,0 +1,12 @@
+class Mentor {
+    var name: String = "Beibarys"
+    var age: Int = 31
+    var height: Double = 187.1
+}
+
+fun main(){
+    var mentor = Mentor()
+    println(mentor.name)
+    println(mentor.age)
+    println(mentor.height)
+}
